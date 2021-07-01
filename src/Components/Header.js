fb93c6dde15d3436e60fe8677abc57d2fb30bebd
kevinmisati <kevinmosigisi2001@gmail.com>
2021-07-01 13:17:27 +0300

    third commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,6 +8,8 @@ const Header = () => {
     const ctx = useContext(Context)
     const cartItemsNumber = ctx.products.length;
     return (
+        <>
+            
         <div className={classes.header}>
             <div className={classes.ad}>
                 <img src="https://ke.jumia.is/cms/2021/JA21/Live/HP/Week2/Day5/Explosion/KE_JA21_ExplosionDay_Tease_D-(1).gif" alt=""/>
@@ -15,8 +17,12 @@ const Header = () => {
             <div className={classes.icons}>
                 <FoodHeaderIcon />
                 <PayIcon />
-            </div>
-            <div className={classes["nav-container"]}>
+                </div>
+                </div>
+                
+                
+            {/* <div  className={classes["nav-container"]}>
+               
                 <nav className={classes.nav}>
                    
                     <Bars />
@@ -52,8 +58,47 @@ const Header = () => {
                     <button>{<SearchIcon />}</button>
                     <input placeholder="Search" type="text" />
                 </div>
+            </div>*/}
+            <div className={classes["nav-container"]}>
+
+                <nav className={classes.nav}>
+
+                    <Bars />
+                    <Link to="/"><Logo /></Link>
+                    <form action="">
+                        <div>
+                            <button className={classes["search-btn-icon"]}><SearchIcon /></button>
+                            <input placeholder="Search products, brands and categories" type="text" />
+                        </div>
+                        <button className={classes.search}>SEARCH</button>
+                    </form>
+                    <div>
+                        <span><AccountIcon /></span>
+                        <span className={classes["help-text"]}>Login</span>
+                        <span className={classes["help-text"]}><DownArrow /></span>
+                    </div>
+                    <div className={classes["help-text"]}>
+                        <span className={classes["help-text"]}><HelpIcon /></span>
+                        <span className={classes["help-text"]}>Help</span>
+                        <span className={classes["help-arrow"]}><DownArrow /></span>
+                    </div>
+                    <div className={classes["cart-icon"]} >
+                        <Link to="/cart">
+                            <span className={classes["icon"]}><CartIcon /></span>
+                            <span className={classes["cart-text"]}>Cart</span>
+                            {!(cartItemsNumber === 0) && <span className={classes["cart-number"]}>{cartItemsNumber}</span>}
+                        </Link>
+                    </div>
+
+
+                </nav>
+                <div className={classes["search-input"]}>
+                    <button>{<SearchIcon />}</button>
+                    <input placeholder="Search" type="text" />
+                </div>
             </div>
-        </div>
+             
+            </>
     )
 }
 
